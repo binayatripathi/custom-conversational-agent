010b71fb174fe6f14d08d285cb2a62afb7eac1c1
binayatripathi <62129561+binayatripathi@users.noreply.github.com>
2023-06-26 16:14:02 -0700

    Update app.py
    opanai streamlit

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import streamlit as st
+import openai
+
+opanai.apy_key = "sk-JGftaPYrPTKm8jnXAcZgT3BlbkFJzf61UE7lsdWWmM0AZdk"
 
 def main():
     st.title("CHAT WITH YOUR DOCUMENT")
     st.write("Welcome to the personal chatbot agent that know whats in your documents!")
     user_input = st.text_input("Ask anything about your dcouments")
     if user_input:
+        st.button("Send")
+        response = opanai.Completion.create(
+            engine = "text-davinci-003",
+            prompt = "Answer the following based on the documents selected" + user_input
+            max_tokens = 3000,
+            temperature = 0.0,
+        )
+
+        res = response["choices"][0]["text"]
         st.write(f"You said: {user_input}")
 
 if __name__ == "__main__":
