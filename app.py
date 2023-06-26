719a8fef7b083c2b1778be2cfbc59002e5fd6e7a
binayatripathi <62129561+binayatripathi@users.noreply.github.com>
2023-06-26 16:14:32 -0700

    Update app.py
    typo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
         st.button("Send")
         response = opanai.Completion.create(
             engine = "text-davinci-003",
-            prompt = "Answer the following based on the documents selected" + user_input
+            prompt = "Answer the following based on the documents selected" + user_input, 
             max_tokens = 3000,
             temperature = 0.0,
         )
