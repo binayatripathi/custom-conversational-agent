0baaa42916664362f6e4beffbed5f7e47d82e87d
binayatripathi <62129561+binayatripathi@users.noreply.github.com>
2023-06-26 15:30:07 -0700

    Update app.py
    typo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title("CHAT WITH YOUR DOCUMENT")
     st.write("Welcome to the personal chatbot agent that know whats in your documents!")
-    user_input = st.text_input("Ask abything about your dcouments")
+    user_input = st.text_input("Ask anything about your dcouments")
     if user_input:
         st.write(f"You said: {user_input}")
 
